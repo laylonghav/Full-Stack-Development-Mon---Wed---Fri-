a8c64d05b0c1659fde5372f14bcbd8f427370a04
laylonghav <laylonghav2023@gmail.com>
2025-09-05 15:33:30 +0700

    done 123456ewwr3456erere

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import counterReduce from "./counterSlice";
 import userReduce from "./userSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const configPersist = {
+  key: "root",
+  storage,
+  whitelist: ["user", "counter"],
+};
+
+const rootReduc = combineReducers({
+  counter: counterReduce,
+  user: userReduce,
+});
+
+const persistedReduc = persistReducer(configPersist, rootReduc);
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReduce,
-    user: userReduce,
-  },
+  reducer: persistedReduc,
 });
+export const persistor = persistStore(store);
